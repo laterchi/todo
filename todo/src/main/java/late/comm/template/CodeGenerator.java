e0b2caf0a9e1a61e03fe3837eb308c6bfae28785
cjj95 <cjj95@LAPTOP-50QHE8BI>
2018-11-22 09:57:24 +0800

    代码生成工具

---
+++ todo/src/main/java/late/comm/template/CodeGenerator.java
@@ -0,0 +1,43 @@
+/**
+ * @description
+ */
+package late.comm.template;
+
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.ResourceLoader;
+
+import late.comm.entity.BaseEntity;
+import late.todo.entity.GlobalTimeEntity;
+
+/**
+ * 代码生成
+ * 
+ * @projectName todo
+ * @packageName late.comm.template
+ * @fileName CodeGenerator.java
+ * @author chijingjia
+ * @createTime :2018年11月21日 下午1:36:16
+ * @version: v1.0
+ */
+public class CodeGenerator {
+	protected static ResourceLoader resourceLoader = new DefaultResourceLoader(); 
+
+	/**
+	 * 
+	 * @methodName main
+	 * @author chijingjia
+	 * @createTime 2018年11月21日 下午1:36:16
+	 * @version v1.0
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		generate(GlobalTimeEntity.class);
+	}
+
+	public static void generate(Class<? extends BaseEntity> clazz) {
+		IServiceGenerator.generate(clazz);
+		ServiceImplGenerator.generate(clazz);
+		RepositoryGenerator.generate(clazz);
+	}
+
+}
---
+++ todo/src/main/java/late/comm/template/IServiceGenerator.java
@@ -0,0 +1,74 @@
+/**
+ * @description
+ */
+package late.comm.template;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.springframework.core.io.Resource;
+
+import late.comm.entity.BaseEntity;
+
+/**
+ * 
+ * @projectName todo
+ * @packageName late.comm.template
+ * @fileName IServiceGenerator.java
+ * @author chijingjia
+ * @createTime :2018年11月21日 下午1:36:51
+ * @version: v1.0
+ */
+public class IServiceGenerator extends CodeGenerator {
+	private static final String TEMPLATE_URL = "codetemplates/IService.template";
+	private static final Map<String, String> REPLACE_PARAM = new HashMap<>();
+
+	public static void generate(Class<? extends BaseEntity> clazz) {
+		FileReader fr = null;
+		BufferedReader br = null;
+		try {
+			String packageName = clazz.getPackage().getName();
+			REPLACE_PARAM.put("PACKAGE_NAME", packageName.substring(0, packageName.length() - ".entity".length()) + ".service");
+			String nickName = clazz.getSimpleName();
+			nickName = nickName.substring(0, nickName.length() - "Entity".length());
+			REPLACE_PARAM.put("NICK_NAME", nickName);
+			nickName = nickName.substring(0, 1).toLowerCase() + nickName.substring(1, nickName.length());
+			REPLACE_PARAM.put("NICK_NAME_LOWER", nickName);
+			Resource resource = resourceLoader.getResource("classpath:" + TEMPLATE_URL);
+			fr = new FileReader(resource.getFile());
+			br = new BufferedReader(fr);
+			String line;
+			StringBuilder codeText = new StringBuilder();
+			while ((line = br.readLine()) != null) {
+				if (line.indexOf("$") > -1) {
+					for (Entry<String, String> entry : REPLACE_PARAM.entrySet()) {
+						line = line.replaceAll("\\$\\{" + entry.getKey() + "\\}", entry.getValue());
+					}
+				}
+				codeText.append(line).append("\n");
+			}
+			System.out.println(codeText);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fr.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+			try {
+				br.close();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
